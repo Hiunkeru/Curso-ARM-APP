a2add1dabdbef9f852165282ef59aaf8a7d1b2e7
Alberto Picazo <apicazo@plainconcepts.com>
2020-09-03 17:52:04 +0200

    add code

---
+++ curso-arm-docker-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace curso_arm_docker_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
